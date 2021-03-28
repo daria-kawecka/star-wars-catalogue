8674872cc636d549d48cc4aad99f757059520ce1
daria-kawecka <dusiek2806@gmail.com>
2021-03-28 23:48:40 +0200

    create global styles

---
+++ src/assets/styles/GlobalStyles.js
@@ -0,0 +1,22 @@
+import { createGlobalStyle } from 'styled-components';
+
+export const GlobalStyles = createGlobalStyle`
+  html {
+    box-sizing: border-box;
+  }
+
+  *, *::after, *::before {
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+  }
+
+  body {
+    font-family: 'Quicksand',sans-serif;
+    background: #151414;
+  }
+
+  a, button {
+    font-family: 'Quicksand',sans-serif;
+  }
+`;
---
+++ src/assets/styles/theme.js
@@ -0,0 +1,19 @@
+export const theme = {
+  colors: {
+    yellow: '#cfbc1f',
+    yellowWithOpacity: 'rgba(207,207,31,0.7)',
+    error: '#CF1F22',
+    dark: '#131212',
+    grey: '#212429',
+    light: '#cdcdcd',
+  },
+  mixins: {
+    backgroundColor: 'radial-gradient(circle, rgba(205,205,205,1) 0%, rgba(106,106,106,1) 100%)',
+  },
+  fontSize: {
+    xl: '32px',
+    l: '26px',
+    m: '22px',
+    s: '16px',
+  },
+};
